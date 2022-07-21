f9623c35945b530dce52aed5ef135588d93e4d54
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-21 23:02:16 +0530

    added. a feature to add new contact

---
+++ AddressBook/AddressBookList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBookList
+    {
+        List<Contact> addresslist = new List<Contact>();
+        public void Addcontact(Contact contact)
+        {
+            addresslist.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addresslist)
+            {
+                Console.WriteLine("firstname =" + contact.firstname);
+                Console.WriteLine("lastname =" + contact.lastname);
+                Console.WriteLine("Address =" + contact.Address);
+                Console.WriteLine("City =" + contact.city);
+                Console.WriteLine("State =" + contact.state);
+                Console.WriteLine("Zip =" + contact.zip);
+                Console.WriteLine("Phone-Number =" + contact.phone_number);
+                Console.WriteLine("Email-Id =" + contact.email_id);
+            }
+
+        }
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,38 +6,60 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM !");
+            Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM");
+            bool flag = true;
+            int option;
+            Contact newcontact = new Contact();
+            AddressBookList add = new AddressBookList();
+            while (flag == true)
+            {
+                Console.WriteLine("CHOOSE OPTION\n1.Create Contact\n2.Add contact in addressbook\n3.exit");
 
-                bool flag = true;
-                while (flag == true)
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
-                    Console.WriteLine("CHOOSE OPTION\n1.Create Contact\n2.Exit");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            Console.WriteLine("create contact in specific order:firstname,lastname,address,city,state,zip,phone number,email-id.");
-                            Contact contact = new Contact
-                            {
-                                firstname = Console.ReadLine(),
-                                lastname = Console.ReadLine(),
-                                Address = Console.ReadLine(),
-                                city = Console.ReadLine(),
-                                state = Console.ReadLine(),
-                                zip = Console.ReadLine(),
-                                phone_number = Console.ReadLine(),
-                                email_id = Console.ReadLine(),
-                            };
-                            break;
-                        case 2:
-                            flag = false;
-                            break;
-                        default:
-                            Console.WriteLine("your choice should be between 1 to 2");
-                            break;
-                    }
+                    case 1:
+                        Console.WriteLine("create contact in specific order:firstname,lastname,address,city,state,zip,phone number,email-id.");
+                        newcontact = new Contact()
+                        {
+                            firstname = Console.ReadLine(),
+                            lastname = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            city = Console.ReadLine(),
+                            state = Console.ReadLine(),
+                            zip = Console.ReadLine(),
+                            phone_number = Console.ReadLine(),
+                            email_id = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        Console.WriteLine("Add contact in specific order:firstname,lastname,address,city,state,zip,phone.no,email-id");
+                        newcontact = new Contact();
+                        add = new AddressBookList();
+                        add.Addcontact(newcontact);
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.Address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phone_number = Console.ReadLine();
+                        newcontact.email_id = Console.ReadLine();
+                        add.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 3");
+                        break;
                 }
             }
         }
     }
+}
+
+
+
+
 
