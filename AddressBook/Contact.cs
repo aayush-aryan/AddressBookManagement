423ee5f7605fbd6bd1cc5bdc37886389780ccf56
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-21 22:27:55 +0530

    added. create contacts features

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -6,6 +6,13 @@ namespace AddressBook
 {
     class Contact
     {
-
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phone_number { get; set; }
+        public string email_id { get; set; }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,37 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM !");
+
+                bool flag = true;
+                while (flag == true)
+                {
+                    Console.WriteLine("CHOOSE OPTION\n1.Create Contact\n2.Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("create contact in specific order:firstname,lastname,address,city,state,zip,phone number,email-id.");
+                            Contact contact = new Contact
+                            {
+                                firstname = Console.ReadLine(),
+                                lastname = Console.ReadLine(),
+                                Address = Console.ReadLine(),
+                                city = Console.ReadLine(),
+                                state = Console.ReadLine(),
+                                zip = Console.ReadLine(),
+                                phone_number = Console.ReadLine(),
+                                email_id = Console.ReadLine(),
+                            };
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("your choice should be between 1 to 2");
+                            break;
+                    }
+                }
+            }
         }
     }
-}
+
